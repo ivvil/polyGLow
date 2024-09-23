87b3f5aa006e1fcf50e119cc1c505df7fbda1f55
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-09-24 00:12:42 +0200

    Holy fucking bingle, a triforce appears

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include "../include/glad/glad.h"
 // #include "KHR/khrplatform.h"
 #include <GLFW/glfw3.h>
-#include <iostream> 
+#include <iostream>
 #include <math.h>
 
 // Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-  "layout (location = 0) in vec3 aPos;\n"
-  "void main()\n"
-  "{\n"
-  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-  "}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "void main()\n"
-  "{\n"
-  "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-  "}\n\0";
-
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+// Fragment Shader source code
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+    "}\n\0";
 
 int main() {
   glfwInit();
@@ -27,21 +28,28 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(800, 800, "polyGLow", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(800, 800, "polyGLow", NULL, NULL);
 
   GLfloat verts[] = {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+      -0.5f,     -0.5f * float(sqrt(3)) / 3,    0.0f, // Lower left corner
+      0.5f,      -0.5f * float(sqrt(3)) / 3,    0.0f, // Lower right corner
+      0.0f,      0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+      -0.5f / 2, 0.5f * float(sqrt(3)) / 6,     0.0f, // Inner left
+      0.5f / 2,  0.5f * float(sqrt(3)) / 6,     0.0f, // Inner right
+      0.0f,      -0.5f * float(sqrt(3)) / 3,    0.0f  // Inner down
   };
 
-
+  GLuint indices[] = {
+      0, 3, 5, // Lower left triangle
+      3, 2, 4, // Lower right triangle
+      5, 4, 1, // Upper triangle
+  };
 
   if (window == NULL) {
-	std::cout << "Erm, what the sigma" << std::endl;
-	glfwTerminate();
+    std::cout << "Erm, what the sigma" << std::endl;
+    glfwTerminate();
 
-	return -1;
+    return -1;
   }
 
   glfwMakeContextCurrent(window);
@@ -66,21 +74,27 @@ int main() {
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  GLuint VAO, VBO;
+  GLuint VAO, VBO, EBO;
 
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
-  
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
   glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 
@@ -89,27 +103,29 @@ int main() {
   glfwSwapBuffers(window);
 
   while (!glfwWindowShouldClose(window)) {
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 
-	glBindVertexArray(VAO);
+    glBindVertexArray(VAO);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
-	glfwSwapBuffers(window);
-	
-	glfwPollEvents();
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
   }
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
   glDeleteShader(shaderProgram);
 
   glfwDestroyWindow(window);
 
   glfwTerminate();
-  
+
   return 0;
 }
